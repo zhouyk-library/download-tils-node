71b8d7f1d68f3361a7f92582030669689a986bc3
zhouyk-library <zhouyk.email@qq.com>
2020-09-19 10:27:50 +0800

    init

--- project.config.js
+++ project.config.js
@@ -1,13 +1,8 @@
-module.exports={
-  url:'http://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}',
-  zooms:[1],
-  maxs:[85,180],
-  mins:[-85,-180],
-  header:{
-  'accept': 'image/webp,*/*',
-  'Referer': 'http://localhost:9527/#/exhibition-center/map-view',
-  'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.34 Safari/537.36 Edg/83.0.478.25'
-  },
-  out:'out',
-  imgtype:'jpeg'
+module.exports = {
+  url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+  zooms: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+  maxs: [36.04731, 119.95358],
+  mins: [34.96493, 118.60232],
+  out: 'out',
+  imgtype: 'png'
 }
